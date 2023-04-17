77ed031a2b7fce4148a7fbb258ce066a03ae25a0
HANEUL32 <ser2070xcv@naver.com>
2023-04-17 15:02:24 +0900

    master

---
+++ java2/src/Files/File2.java
@@ -0,0 +1,18 @@
+package Files;
+
+import java.io.FileReader;
+
+public class File2 {
+
+	public static void main(String[] args) throws Exception {
+		try {
+		FileReader fr = new FileReader("E:\\git_java\\javaproject\\java2\\src\\Files\\message.txt");
+		System.out.println(fr.getEncoding()); //파일 인코딩 언어 확인
+		System.out.println(fr.read()); //데이터 크기 
+		fr.close(); //로드한 파일을 닫을 때 사용
+		}
+		catch (Exception e) {
+			System.out.println("파일 경로 실패");
+		}
+	}
+}
