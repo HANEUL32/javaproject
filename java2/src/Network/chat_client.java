f1c6b880addea607150a059e62fa9b682348b723
HANEUL32 <ser2070xcv@naver.com>
2023-04-20 15:20:58 +0900

    master

--- java2/src/Network/chat_client.java
+++ java2/src/Network/chat_client.java
@@ -9,16 +9,61 @@ import java.util.Scanner;
 public class chat_client {
 	//채팅 클라이언트
 	public static void main(String[] args) {
-		
+		chat_cl ch = new chat_cl();
+		ch.client();
 
 	}
 }
 class chat_cl{
 	private String ip = "192.168.10.155";
 	private int port = 10001;
-	ServerSocket sk = null;
 	Socket so = null;
 	Scanner sc = null;
 	InputStream is = null;
 	OutputStream os = null;
+	private String mid = "";
+	private String msg = ""; //메세지 출력
+	
+	public void client() {
+		int count = 0;
+		try {
+			this.sc = new Scanner(System.in);
+			System.out.println("login id:");
+			this.mid = this.sc.nextLine();	
+			while(true) {
+				this.so = new Socket(this.ip,this.port);
+				this.is = this.so.getInputStream(); //서버에서 받아오는 메세지
+				this.os = this.so.getOutputStream(); //서버로 보내는 메세지
+				if(count==0) {
+				this.msg = "["+this.mid+"] Go in";
+				}
+				else {
+					this.os.write(this.msg.getBytes());
+					System.out.println("message: ");
+					String cms = this.sc.nextLine();
+					this.msg = "["+this.mid+"]:" + cms;
+					if(cms.equals("exit")) {
+						this.msg = "chatroom out";
+						this.is.close();
+						this.sc.close();
+						this.so.close();
+					}
+					else {
+						this.msg = "["+this.mid+"]:" + cms;
+					}
+				}
+				count++;			
+				this.os.write(this.msg.getBytes());
+				this.os.flush();
+				byte msg[] = new byte[1024];
+				int n = this.is.read(msg);
+				String servertext = new String(msg,0,n);
+				System.out.println(servertext);
+			}
+			
+		}
+		catch (Exception e) {
+			System.out.println("Server Connect Error!!");
+		}
+	}
 }
--- java2/src/Network/chat_server.java
+++ java2/src/Network/chat_server.java
@@ -9,7 +9,8 @@ import java.util.Scanner;
 public class chat_server {
 	//채팅 서버
 	public static void main(String[] args) {
-
+		chat_sv cs = new chat_sv();
+		cs.chatserver();
 	}
 }
 class chat_sv{
@@ -19,5 +20,38 @@ class chat_sv{
 	Scanner sc = null;
 	InputStream is = null;
 	OutputStream os = null;
-	
+	private String mid = null;
+	public void chatserver() {
+		try {
+			this.sk = new ServerSocket(this.port);
+			this.sc = new Scanner(System.in);
+			System.out.println("login id : "); //방 개설자 아이디
+			this.mid = sc.next();
+			System.out.println("Chating Room Open");
+			while(true) {
+				this.so = sk.accept();
+				this.is = this.so.getInputStream(); //클라이언트에게 받는 메세지
+				this.os = this.so.getOutputStream(); //클라이언트에게 보내는 메세지
+				byte msg[] = new byte[1024];
+				int n = this.is.read(msg);
+				String text = new String(msg,0,n);
+				System.out.println(text); //클라이언트 입장 메세지(채팅 메세지)
+				this.sc = new Scanner(System.in);
+				System.out.println("Server message: ");
+				text = this.sc.nextLine();
+				if(text.equals("exit")) {
+					this.sk.close();
+					this.so.close();
+					break;
+				}
+				
+				String cms = "["+this.mid+"]:" + text;
+				this.os.write(cms.getBytes());
+				this.os.flush();
+			}
+		}
+		catch (Exception e) {
+			System.out.println("Port Connect Error!!");
+		}
+	}
 }
