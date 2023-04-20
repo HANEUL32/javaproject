caf0ea72a74210bac78a7709c39c0c95a92fb65a
HANEUL32 <ser2070xcv@naver.com>
2023-04-20 12:40:28 +0900

    master

---
+++ java2/src/Network/chat_client.java
@@ -0,0 +1,24 @@
+package Network;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class chat_client {
+	//채팅 클라이언트
+	public static void main(String[] args) {
+		
+
+	}
+}
+class chat_cl{
+	private String ip = "192.168.10.155";
+	private int port = 10001;
+	ServerSocket sk = null;
+	Socket so = null;
+	Scanner sc = null;
+	InputStream is = null;
+	OutputStream os = null;
+}
---
+++ java2/src/Network/chat_server.java
@@ -0,0 +1,23 @@
+package Network;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class chat_server {
+	//채팅 서버
+	public static void main(String[] args) {
+
+	}
+}
+class chat_sv{
+	private int port = 10001;
+	ServerSocket sk = null;
+	Socket so = null;
+	Scanner sc = null;
+	InputStream is = null;
+	OutputStream os = null;
+	
+}
